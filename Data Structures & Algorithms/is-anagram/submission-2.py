c1972d7364525e7aba338508c21d2959c46357fe
Kartik Soni <kartiksoni2209@gmail.com>
2026-06-20 17:57:40 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        # 1. hashmap with counts of characters. 2. sorting
+        freq_s = {}
+        freq_t = defaultdict(int)
+        # 1. build frequency maps
+        for c in s:
+            freq_s[c] = freq_s.get(c, 0) +1
+        
+        for c in t:
+            freq_t[c] += 1
+
+        # iterate over keys in s, compare with frequencies in t
+        for c in freq_s.keys():
+            freq_t[c] -= freq_s[c]
+            if freq_t[c] != 0:
+                return False
+        
+        for c in freq_t.keys():
+            if freq_t[c] != 0:
+                return False
+
+        return True
